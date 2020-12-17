5fbe5d9a9323e083774f78a4f00e2eb5205faf4a
ShirokoryadovaK <shirokoryadovak@gmail.com>
2020-12-17 17:24:26 +0300

    form done

--- js/main.js
+++ js/main.js
@@ -19,3 +19,65 @@ let mySwiper = new Swiper(slider, {
 		clickable: true,
 	},
 });
+
+//Form
+const btns = document.querySelectorAll('.btn-form');
+const modals = document.querySelector('.form');
+const modalOverlay = document.querySelector('.form__overlay');
+
+btns.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('.data-path');
+    modals.style.visibility = 'visible';
+    modals.style.opacity = '1';
+    $('body').addClass('scroll');
+  });
+});
+
+modalOverlay.addEventListener('click', (e) => {
+  if (e.target == modalOverlay) {
+    modals.style.visibility = 'hidden';
+    modals.style.opacity = '0';
+    modals.style.transition = 'opacity ease-in-out 0.4s';
+    $('body').removeClass('scroll');
+  }
+});
+
+$(document).ready(function(){
+  $('input[type="tel"]').inputmask({"mask": "+7 (999) 999-9999"});
+
+  $('form').each(function () {
+    $(this).validate({
+      errorPlacement(error, element) {
+        return true;
+      },
+      focusInvalid: false,
+      rules: {
+        Телефон: {
+          required: true,
+        },
+        Имя: {
+          required: true,
+        },
+        Почта: {
+          required: true,
+        }
+      },
+      submitHandler(form) {
+        let th = $(form);
+
+        $.ajax({
+          type: 'POST',
+          url: 'mail.php',
+          data: th.serialize(),
+        }).done(() => {
+          $(function(){
+            $('.form__container').hide(800);
+            $('.alert').show('slow');
+          });
+        });
+        return false;
+      }
+    });
+  });
+});
